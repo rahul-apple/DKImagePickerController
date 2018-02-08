21d57b242f604a769b8251be7ae3637e79d343f0
Rahul Ramachandra <rahul-apple@users.noreply.github.com>
2018-02-08 12:48:18 +0530

    Sample

--- Sources/DKImagePickerController/View/DKAssetGroupDetailVC.swift
+++ Sources/DKImagePickerController/View/DKAssetGroupDetailVC.swift
@@ -418,7 +418,7 @@ open class DKAssetGroupDetailVC: UIViewController,
         }
     }
     
-    public func collectionView(_ collectionView: UICollectionView, shouldSelectItemAt indexPath: IndexPath) -> Bool {
+    open func collectionView(_ collectionView: UICollectionView, shouldSelectItemAt indexPath: IndexPath) -> Bool {
         if let asset = (collectionView.cellForItem(at: indexPath) as? DKAssetGroupDetailBaseCell)?.asset {
             return self.imagePickerController.canSelect(asset: asset)
         } else {
@@ -426,7 +426,7 @@ open class DKAssetGroupDetailVC: UIViewController,
         }
     }
     
-    public func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+    open func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         if self.isCameraCell(indexPath: indexPath) {
             collectionView .deselectItem(at: indexPath, animated: false)
             if UIImagePickerController.isSourceTypeAvailable(.camera) {
@@ -437,7 +437,7 @@ open class DKAssetGroupDetailVC: UIViewController,
         }
     }
     
-    public func collectionView(_ collectionView: UICollectionView, didDeselectItemAt indexPath: IndexPath) {
+    open func collectionView(_ collectionView: UICollectionView, didDeselectItemAt indexPath: IndexPath) {
         self.deselectAsset(atIndex: indexPath)
     }
     
